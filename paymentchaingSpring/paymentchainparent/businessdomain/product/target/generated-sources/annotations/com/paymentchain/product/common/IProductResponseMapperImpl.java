f946a883aba8cc9dc873a7e4dea799ec0f9e15d5
Kmafh <artadapt@gmail.com>
2025-04-23 14:53:38 +0200

    Prepramos ApiGateway y dockeamos

--- paymentchaingSpring/paymentchainparent/businessdomain/product/target/generated-sources/annotations/com/paymentchain/product/common/IProductResponseMapperImpl.java
+++ paymentchaingSpring/paymentchainparent/businessdomain/product/target/generated-sources/annotations/com/paymentchain/product/common/IProductResponseMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-04-23T11:04:49+0200",
+    date = "2025-04-23T14:50:10+0200",
     comments = "version: 1.6.3, compiler: javac, environment: Java 23.0.2 (Oracle Corporation)"
 )
 @Component
---
+++ paymentchaingSpring/paymentchainparent/infraestructuradomain/apigateway/src/main/java/com/paymentchain/setups/GlobalPreFIltering.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.paymentchain.setups;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ *
+ * @author Pc
+ */
+@Slf4j
+public class GlobalPreFIltering implements GlobalFilter{
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("Global prefilter executed");
+        return chain.filter(exchange);
+    }
+    
+}
