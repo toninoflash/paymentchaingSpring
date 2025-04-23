ffa8b77db7b1da475612fae6d81d79d216be6523
Kmafh <artadapt@gmail.com>
2025-04-23 18:03:27 +0200

    Metemos keaycloack para el token

---
+++ paymentchaingSpring/paymentchainparent/infraestructuradomain/kaycloackAdapter/src/test/java/com/paymentchain/KaycloackAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymentchain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaycloackAdapterApplicationTests {
+
+	/*@Test
+	void contextLoads() {
+	}*/
+
+}
